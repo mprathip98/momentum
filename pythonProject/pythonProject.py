499aec63198c6cf87ba28ea7bfd80df494f74100
Mohit Prathipati <prathipati.mohit@gmail.com>
2025-05-23 08:06:04 -0700

    initialized the database + added a new table + still facing a lot of errors when trying to add data

--- pythonProject/pythonProject.py
+++ pythonProject/pythonProject.py
@@ -1,5 +1,6 @@
 import reflex as rx
 from pygments.styles.dracula import background
+from reflex.components.radix.primitives.form import FormSubmit
 
 from rxconfig import config
 
@@ -7,6 +8,8 @@ from rxconfig import config
 class State(rx.State):
     pass
 
+
+#----------HOME PAGE------------------
 def navbar():
     return rx.box(
         rx.desktop_only(
@@ -70,10 +73,8 @@ class Spline(rx.Component):
     scene: rx.Var[str]
 
 spline = Spline.create
-
 #link for the animation on Spline
 scene = "https://prod.spline.design/WgCVJEkCUF1CFaaP/scene.splinecode"
-
 
 def index() -> rx.Component:
     return rx.box(
@@ -137,12 +138,12 @@ def index() -> rx.Component:
         border_color="white",
         height="600px",
     )
+#------------HOME PAGE----------------
 
 
 
 
-
-
+#navbar with just the logo, routing to the home page
 def navbar_plain():
     return rx.box(
         rx.desktop_only(
@@ -176,102 +177,141 @@ def navbar_plain():
         width="100%",
     )
 
+#defining the table - an structure to store the sign up information user
+class userSignUpModel(rx.Model, table=True):
+    name: str
+    username: str
+    password: str
+
+#handling the signUp info
+class signUpState(rx.State):
+    form_data :dict = {}
+
+    @rx.event
+    def handle_submit(self, form_data: dict):
+        print(form_data)
+        with rx.session() as session:
+            db_entry = userSignUpModel(
+                **form_data
+            )
+            session.add(db_entry)
+            session.commit()
+            self.did_submit = True
+            yield
+
+
+
+
 def signUp() -> rx.Component:
+
     return rx.box(
         navbar_plain(),
         rx.card(
             rx.center(
-                rx.vstack(
-                    rx.color_mode_cond(
-                        dark=rx.image(
-                            src="/momentumLogo.png",
-                            alt="Reflex Logo light",
-                            height="4em",
-                            margin_top="5%"),
-                        light=rx.image(
-                            src="/momentumLogoBlack.png",
-                            alt="Reflex Logo dark",
-                            height="4em",
-                            margin_top="5%"),
-                    ),
-
-
-
-                    rx.heading("Create an account", size="5", margin_bottom="20px"),
-
+                rx.form(
                     rx.vstack(
-                        rx.text(
-                            "name",
-                            size="4",
-                            text_align="left",
-                            weight="medium",
-                            width="100%"
+                        rx.color_mode_cond(
+                            dark=rx.image(
+                                src="/momentumLogo.png",
+                                alt="Reflex Logo light",
+                                height="4em",
+                                margin_top="5%"),
+
+                            light=rx.image(
+                                src="/momentumLogoBlack.png",
+                                alt="Reflex Logo dark",
+                                height="4em",
+                                margin_top="5%"),
                         ),
-                        rx.input(
-                            placeholder="John Doe",
-                            width="300px",
+
+                        rx.heading("Create an account", size="5", margin_bottom="20px"),
+
+                        rx.vstack(
+                            rx.text(
+                                "name *",
+                                size="4",
+                                text_align="left",
+                                weight="medium",
+                                width="100%"
+                            ),
+                            rx.input(
+                                name="name",
+                                required=True,
+                                placeholder="John Doe",
+                                type="text",
+                                width="300px",
+                                size="3",
+                                margin="-2%"
+                            ),
+                            margin="2%",
+                        ),
+                        rx.vstack(
+                            rx.text(
+                                "username *",
+                                size="4",
+                                text_align="left",
+                                weight="medium",
+                                width="100%"
+                            ),
+                            rx.input(
+                                type="text",
+                                name="username",
+                                required=True,
+                                placeholder="bigManJohn",
+                                width="300px",
+                                size="3",
+                                margin="-2%"
+                            ),
+                            margin="2%",
+                        ),
+
+                        rx.vstack(
+                            rx.text(
+                                "password *",
+                                size="4",
+                                required=True,
+                                name="password",
+                                text_align="left",
+                                weight="medium",
+                                width="100%"
+                            ),
+                            rx.input(
+                                width="300px",
+                                size="3",
+                                margin="-2%",
+                                type="password",
+                            ),
+                            margin="2%",
+                            margin_bottom="10%",
+                        ),
+                        rx.button(
+                            "Create",
+                            width="30%",
                             size="3",
-                            margin="-2%"
+                            margin_left="-3%",
+                            align="center",
+                            font_weight="bold",
+                            font_size="1em",
+                            type="submit",
                         ),
-                        margin="2%",
-                    ),
-                    rx.vstack(
-                        rx.text(
-                            "username",
-                            size="4",
-                            text_align="left",
-                            weight="medium",
-                            width="100%"
+                        rx.link(
+                            "I already have an account tho...",
+                            margin="3%",
+                            margin_bottom="5%",
+                            href="/login",
+                            width="100%",
+                            text_align="center",
                         ),
-                        rx.input(
-                            placeholder="bigManJohn",
-                            width="300px",
-                            size="3",
-                            margin="-2%"
-                        ),
-                        margin="2%",
-                    ),
-                    rx.vstack(
-                        rx.text(
-                            "password",
-                            size="4",
-                            text_align="left",
-                            weight="medium",
-                            width="100%"
-                        ),
-                        rx.input(
-                            width="300px",
-                            size="3",
-                            margin="-2%",
-                            type="password",
-                        ),
-                        margin="2%",
-                        margin_bottom="10%",
-                    ),
-                    rx.button(
-                        "Create",
-                        width="65%",
-                        size="3",
+
                         align="center",
-                        font_weight="bold",
-                        font_size="1em",
-                        type="submit"
-
-                    ),
-                    rx.link(
-                        "I already have an account tho...",
-                        margin="3%",
-                        margin_bottom="5%",
-                        href="/login",
-                        width="100%",
-                        text_align="center",
+                        border_color="white",
                     ),
 
                     align="center",
-                    border_color = "white",
-                ),
+                    #on_submit=signUpState.handle_submit(),
+                    #reset_on_submit=True,
 
-                align="center",
+                ),
             ),
             width="35%",
             align="center",
@@ -336,7 +376,6 @@ def signIn() -> rx.Component:
                         text_align="center",
                         margin_top="3%",
                         margin_bottom="5%",
-
                     ),
 
                     align="center",
@@ -361,3 +400,4 @@ app = rx.App()
 app.add_page(index)
 app.add_page(signUp, route="/signUp")
 app.add_page(signIn, route="/login")
+
